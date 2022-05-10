17db028a10e947b8fe69df7f159c42c4f3dd2042
Mateo Cañavera <mcanavera@unal.edu.co>
2022-05-10 06:39:11 -0500

    d

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,35 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo{
+	
+	private int puestos;
+	private static int cantidadAutomoviles;
+	
+	
+		
+		public int getPuestos() {
+			return puestos;
+		}
+		public void setPuestos(int puestos) {
+			this.puestos = puestos;
+		}
+		
+		
+		
+	public Automovil(String placa,String nombre,int precio,int peso, Fabricante fabricante,int puestos) {
+		super(placa,4,100,nombre,precio,peso,"FWD",fabricante);
+		this.puestos=puestos;
+		cantidadAutomoviles ++;
+			
+		}
+	public static int getCantidadAutomoviles() {
+		return cantidadAutomoviles;
+	}
+	
+	
+	
+
+	
+	
+
+}
